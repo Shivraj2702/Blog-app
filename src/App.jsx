263af447f77c9797cd860dc6884f260dd4c35858
shivraj <dhotreshivraj206@gmail.com>
2023-11-11 06:26:39 -0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
-import authService from "./appwrite/auth"
+import authService from "./Appwrite/auth"
 import {login, logout} from "./store/authSlice"
 import {Header ,Footer}  from './components'
 import { Outlet } from 'react-router-dom'
