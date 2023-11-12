2aa8839a0120fc29a2bf9529dd66aacb3dbfd980
shivraj <dhotreshivraj206@gmail.com>
2023-11-11 23:21:08 -0800

    path changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import {Link, useNavigate} from 'react-router-dom'
 import { login as authLogin } from '../store/authSlice'
 import {Button, Input, } from "./index"
 import {useDispatch} from "react-redux"
-import authService from "../appwrite/auth"
+import authService from "../Appwrite/auth"
 import {useForm} from "react-hook-form"
 
 function Login() {
